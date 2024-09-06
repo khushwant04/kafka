7a3a9cfbc672f861c1c14d665aaa1c9813016f47
khushwant04 <khushwantsanwalot2004@outlook.com>
2024-09-06 12:33:01 +0000

    commit

---
+++ cmak-3.0.0.7/bin/kafka_manager_utils_one-10_log-config.bat
@@ -0,0 +1,5 @@
+@REM Forwarder script
+
+@echo off
+
+%0\..\cmak -main kafka.manager.utils.one10.LogConfig %*
---
+++ cmak-3.0.0.7/bin/kafka_manager_utils_zero-90_log-config.bat
@@ -0,0 +1,5 @@
+@REM Forwarder script
+
+@echo off
+
+%0\..\cmak -main kafka.manager.utils.zero90.LogConfig %*
